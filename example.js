110d828d50d89c58d3462c7958f845a07c927db2
Ronnie Jaanhold <ronnie@cybers.eu>
2020-03-13 12:08:04 +0200

    commit

--- example.js
+++ example.js
@@ -7,5 +7,20 @@ const argv = require('optimist')
 
 const list = new Generator(argv._)
 
+var start = new Date()
+var hrStart = process.hrtime()
+
+
+
     for(c = 0; c < argv.count; c++)
-        console.log(list.generateNonsense(argv.listName))
+        list.generateNonsense(argv.listName)
+
+//        console.log(new Date().getTime() + '. ' + list.generateNonsense(argv.listName))
+
+
+var end = new Date() - start
+var hrEnd = process.hrtime(hrStart)
+
+console.info('Execution time: %dms', end)
+console.info('Execution time (hr): %ds %dms', hrEnd[0], hrEnd[1] / 1000000)
+
